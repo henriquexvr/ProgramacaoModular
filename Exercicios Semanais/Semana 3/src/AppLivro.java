e61ec1c2af12f6cb6432f21516259ccd548a2c10
henriquexvr <henriquecaldeirax3@gmail.com>
2026-03-09 16:04:18 -0300

    Exercicios das semanas 3 (Classe Livro e App Livro) e 4 (Classe Pizza e App Pizzaria)

--- Exercicios Semanais/Semana 3/src/AppLivro.java
+++ Exercicios Semanais/Semana 3/src/AppLivro.java
@@ -6,11 +6,9 @@ void main(){
     String nomeAutor = IO.readln("Qual o nome do autor livro? ");
     livro1.autor = nomeAutor;
     String numeroPaginasStr = IO.readln("Quantas paginas tem o livro? ");
+    livro1.numPags = numeroPags = Integer.parseInt(numeroPaginasStr);
     String qtndAvaliacoesSrt = IO.readln("Quantas pessoas leram esse livro? ");
     qtndAvalicoes = Integer.parseInt(qtndAvaliacoesSrt);
-    numeroPags = Integer.parseInt(numeroPaginasStr);
-    livro1.numPags = numeroPags;
-    
     IO.println("Tamanho do livro: "+ livro1.tamanhoLivro(numeroPags));
     IO.println("Media de avaliacoes: "+livro1.avaliacaoMedia(qtndAvalicoes) +" estrelas");
 }
